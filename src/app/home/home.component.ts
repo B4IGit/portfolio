719d2e37f200e66725e2a6fd161635d9cff9794d
Devin Ledesma <dev.dfledesma@gmail.com>
2025-09-24 15:18:45 -0500

    Styled code container; Added social media links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,37 +6,90 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <section class="hero flex min-w-0 p-6">
+    <div class="hero flex min-w-0 p-6">
       <!-- Left: 50% -->
       <div class=" w-1/2 p-6">
-        <h1 class="text-4xl font-bold">
-          Hello, <br />
-          I am <span class="text-[var(--primary)]">Devin Ledesma</span>, I'm a
-          Professional <span class="text-[var(--tertiary)]">Full-Stack</span>
-          <span class="text-[var(--secondary)]"> Web Developer</span>.
-        </h1>
+        <!-- Title -->
+        <div>
+          <h1 class="text-4xl font-bold">
+            Hello, <br />
+            I am <span class="text-[var(--primary)]">Devin Ledesma</span>, I'm a
+            Professional <span class="text-[var(--tertiary)]">Full-Stack</span>
+            <span class="text-[var(--secondary)]"> Web Developer</span>.
+          </h1>
+        </div>
+        <!-- Social links -->
+        <div class="flex items-center gap-6 mt-10">
+          <a href="https://github.com/b4igit">
+            <svg
+              class="w-12 h-12 fill-current"
+              role="img"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+              />
+            </svg>
+          </a>
+          <a href="https://www.linkedin.com/in/devin-f-ledesma/">
+            <svg
+              class="w-12 h-12 fill-current"
+              height="100%"
+              style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+              version="1.1"
+              viewBox="0 0 512 512"
+              width="100%"
+              xml:space="preserve"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:serif="http://www.serif.com/"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+            >
+              <path
+                d="M449.446,0c34.525,0 62.554,28.03 62.554,62.554l0,386.892c0,34.524 -28.03,62.554 -62.554,62.554l-386.892,0c-34.524,0 -62.554,-28.03 -62.554,-62.554l0,-386.892c0,-34.524 28.029,-62.554 62.554,-62.554l386.892,0Zm-288.985,423.278l0,-225.717l-75.04,0l0,225.717l75.04,0Zm270.539,0l0,-129.439c0,-69.333 -37.018,-101.586 -86.381,-101.586c-39.804,0 -57.634,21.891 -67.617,37.266l0,-31.958l-75.021,0c0.995,21.181 0,225.717 0,225.717l75.02,0l0,-126.056c0,-6.748 0.486,-13.492 2.474,-18.315c5.414,-13.475 17.767,-27.434 38.494,-27.434c27.135,0 38.007,20.707 38.007,51.037l0,120.768l75.024,0Zm-307.552,-334.556c-25.674,0 -42.448,16.879 -42.448,39.002c0,21.658 16.264,39.002 41.455,39.002l0.484,0c26.165,0 42.452,-17.344 42.452,-39.002c-0.485,-22.092 -16.241,-38.954 -41.943,-39.002Z"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
       <!-- Right: 50% -->
-      <div class="w-1/2">
-        <div class="code-container mx-auto w-4/5 bg-[var(--bg)] rounded">
+      <div class="w-1/2 p-2">
+        <div class="code-container mx-auto bg-[var(--bg)] rounded">
           <div class="code-navbar border-b-2 p-3 flex gap-1.5">
             <div class="w-3 h-3 rounded-full bg-[var(--primary)]"></div>
             <div class="w-3 h-3 rounded-full bg-[var(--secondary)]"></div>
             <div class="w-3 h-3 rounded-full bg-[var(--tertiary)]"></div>
           </div>
-          <div class="code-content border-box">
+          <div class="code-content border-box p-2">
             <pre ngNonBindable>
-                  <code>
-                  const currentStudent = &#123;
-                    name: 'Devin Ledesma',
-                    skills: ['Angular', 'HTML5', 'Tailwind', 'Node.js', 'Express', 'MongoDB'],
-                  &#125;
-                </code>
-                </pre>
+               <code>
+                  <span class="ml-2 text-[var(--const)]">const </span> currentStudent <span class="text-[var(--const)]">=</span> <span class="text-[var(--symbol)]">&#123;</span>
+                    <span class="ml-6">name:</span> <span class="text-[var(--symbol)]">'<span class="text-[var(--string)]">Devin Ledesma</span>'</span>,
+                  <span class="ml-6">status:</span> <span class="text-[var(--symbol)]">'<span class="text-[var(--string)]">Web Development Student</span>'</span>,
+                 <span class="ml-6">skills:</span> <span class="text-[var(--symbol)]">['<span class="text-[var(--skill)]">Angular</span>'</span>, <span class="text-[var(--symbol)]">'<span class="text-[var(--skill)]">HTML5</span>'</span>, <span class="text-[var(--symbol)]">'<span class="text-[var(--skill)]">Tailwind</span>'</span>, <span class="text-[var(--symbol)]">'<span class="text-[var(--skill)]">Node.js</span>'</span>, <span class="text-[var(--symbol)] ml-6">'<span class="text-[var(--skill)]">Express</span>'</span>, <span class="text-[var(--symbol)]">'<span class="text-[var(--skill)]">MongoDB</span>']</span>,
+                  <span class="ml-6">traits:</span> <span class="text-[var(--symbol)]">&#123;</span>
+                  <span class="ml-12">hardWorker:</span> <span class="text-[var(--boolean)]">true</span>,
+                  <span class="ml-12">eagerToLearn:</span> <span class="text-[var(--boolean)]">true</span>,
+                  <span class="ml-12">problemSolver:</span> <span class="text-[var(--boolean)]">true</span>,
+                  <span class="text-[var(--symbol)] ml-6">&#125;</span>,
+                  <span class="text-[var(--const)] ml-6">readyForCareer:</span><span class="text-[var(--boolean)]">function</span><span class="text-[var(--symbol)]">() &#123;</span>
+                    <span class="text-[var(--string)] ml-12">return</span>
+                  <span class="text-[var(--boolean)] ml-12">this</span>.traits.hardWorker <span class="text-[var(--string)]">&&</span>
+                   <span class="text-[var(--boolean)] ml-12">this</span>.traits.problemSolver <span class="text-[var(--string)]">&&</span>
+                   <span class="text-[var(--boolean)] ml-12">this</span>.skills.length >= <span class="text-[var(--skill)]">4</span>;
+                  <span class="text-[var(--symbol)] ml-6">&#125;</span>
+                <span class="text-[var(--tertiary)] ml-2">&#125;</span>;
+                <span class="ml-2">if</span> <span class="text-[var(--tertiary)]">(</span>currentStudent.<span class="text-[var(--const)]">readyForCareer</span><span class="text-[var(--tertiary)]">()) &#123;</span>
+                 <span class="text-[var(--skill)] ml-6">console.log</span><span class="text-[var(--symbol)]">('</span>Ready to take on real-world challenges!<span class="text-[var(--symbol)]">')</span>;
+                <span class="text-[var(--symbol)] ml-2">&#125;</span> else <span class="text-[var(--symbol)]">&#123;</span>
+                  <span class="text-[var(--skill)] ml-6">console.log</span>('Still learning new skills!<span>')</span>;
+                <span>&#125;</span>
+              </code>
+            </pre>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   `,
   styles: `
     .code-container {
@@ -45,6 +98,15 @@ import { Component } from '@angular/core';
       background:
         var(--bg-dark) padding-box,
         var(--border-gradient) border-box;
+    }
+
+    .code-content code {
+      display: block;
+      max-width: 100%;
+    }
+
+    svg {
+      fill: var(--primary);
     }
   `,
 })
