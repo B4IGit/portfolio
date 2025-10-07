1ffe55c0f903c05f05a2210b6d4d9a06c9738919
Devin Ledesma <dev.dfledesma@gmail.com>
2025-10-06 22:10:13 -0500

    Initial commit

---
+++ src/app/education/education.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  standalone: true,
+  imports: [],
+  template: `<!-- Education -->
+    <section class="bg-[var(--bg)]"></section>`,
+  styles: ``,
+})
+export class EducationComponent {}
