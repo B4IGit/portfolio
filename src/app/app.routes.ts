ba8a9286b5c2c2fafc08e70f98fcaa73cea36828
Devin Ledesma <dev.dfledesma@gmail.com>
2025-10-06 22:13:19 -0500

    Added paths for about, skills, projects, education, and contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   {
@@ -9,5 +13,25 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'skills',
+    component: AboutComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'education',
+    component: EducationComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
