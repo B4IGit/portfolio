655aaef193e3aed9f8628eb0b770af3e9cab30d2
Devin Ledesma <dev.dfledesma@gmail.com>
2025-10-10 11:26:55 -0500

    Updated education component: refactored layout to use grid for improved responsiveness and cleaner structure.

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -6,43 +6,43 @@ import 'tailwindcss';
   standalone: true,
   imports: [],
   template: `<!-- Education -->
-    <section class="">
-      <div class="wrapper flex flex-col md:flex-row min-w-0 gap-6">
-        <div class="w-full md:w-1/2 justify-center items-center">
-          <h3 class="w-full py-3 text-2xl bg-[var(--bg)] text-[var(--primary)]">
-            &lt;!&ndash; Education &ndash;&gt;
+    <section
+      class="grid relative gap-12 md:grid-cols-2 md:items-center md:gap-6 md:py-8"
+    >
+      <div>
+        <h3 class="w-full py-3 text-2xl bg-[var(--bg)] text-[var(--primary)]">
+          &lt;!&ndash; Education &ndash;&gt;
+        </h3>
+        <img
+          src="/portfolio/assets/undraw_deep-work_muov.svg"
+          alt="deep work"
+        />
+      </div>
+
+      <!-- Right side -->
+      <div class="space-y-6 md:space-y-8">
+        <!-- Education Card 1 -->
+        <div
+          class="grid grid-rows-[auto,1fr,auto] py-4 card-wrapper w-full max-w-md bg-[var(--bg)] shadow-lg rounded-lg p-6 text-center md:text-left"
+        >
+          <p class="text-[var(--primary)] pb-4 md:text-left">2025 - 2026</p>
+          <h3 class="text-xl font-semibold pb-2 text-[var(--secondary)]">
+            Full-Stack Web Development, BS
           </h3>
-          <img
-            src="/portfolio/assets/undraw_deep-work_muov.svg"
-            alt="deep work"
-          />
+          <p class="text-[var(--text)]">Bellevue University, Bellevue, NE</p>
         </div>
 
-        <!-- Right side -->
+        <!-- Education Card 2 -->
         <div
-          class="w-full md:w-1/2 flex flex-col items-center justify-center space-y-6 px-4"
+          class="grid grid-rows-[auto,1fr,auto] py-4 card-wrapper w-full max-w-md bg-[var(--bg)] shadow-lg rounded-lg p-6 text-center md:text-left"
         >
-          <!-- Education Card 1 -->
-          <div
-            class=" card-wrapper w-full max-w-md bg-[var(--bg)] border-b-2 shadow-lg rounded-lg p-6"
-          >
-            <h3 class="text-xl font-semibold text-[var(--secondary)] mb-2">
-              Full-Stack Web Development, BS
-            </h3>
-            <p class="text-[var(--text)]">Bellevue University, Bellevue, NE</p>
-          </div>
-
-          <!-- Education Card 2 -->
-          <div
-            class=" card-wrapper w-full max-w-md bg-[var(--bg)] shadow-lg rounded-lg p-6"
-          >
-            <h3 class="text-xl font-semibold text-[var(--secondary)] mb-2">
-              Web Development, AAS
-            </h3>
-            <p class="text-[var(--text)]">
-              Des Moines Area Community College, Ankeny, IA
-            </p>
-          </div>
+          <p class="text-[var(--primary)] pb-4 md:text-left">2023 - 2025</p>
+          <h3 class="text-xl font-semibold pb-2 text-[var(--secondary)]">
+            Web Development, AAS
+          </h3>
+          <p class="text-[var(--text)]">
+            Des Moines Area Community College, Ankeny, IA
+          </p>
         </div>
       </div>
     </section>`,
