850b880ecd1cb274b02b3e8f303a0b9ad6fa97b9
Devin Ledesma <dev.dfledesma@gmail.com>
2025-10-10 16:41:41 -0500

    Refactored about component: updated layout to grid for enhanced responsiveness, added project showcase with styling improvements, and revised content structure.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,44 +4,191 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   standalone: true,
   imports: [],
-  template: ` <section class="flex items-center min-h-screen">
-    <div class="w-full p-2">
-      <!-- Full-width heading -->
+  template: ` <section
+    class="grid relative border-box gap-12 md:grid-cols-2 md:items-center md:gap-10 md:py-8"
+  >
+    <div class="bg-[var(--bg)]">
+      <h3 class="text-2xl text-[var(--primary)] pb-4 md:pb-8">
+        &lt;!&ndash; About Me &ndash;&gt;
+      </h3>
+      <div class="text-[var(--text-dark)] text-lg">
+        <p>
+          I'm a passionate Web Developer based in Iowa, currently immersed in
+          studying Web Development. Alongside my coursework, I juggle two jobs
+          to support the lavish lifestyle of my two beloved cats. I'm driven by
+          curiosity and creativity, and I look forward to expanding my skills
+          even further, especially once I complete my studies. I'm looking
+          forward to exploring new opportunities to enhance my skills.
+        </p>
+        <br />
+        <p>
+          I have a strong passion for UI/UX design, graphic design, 3D modeling,
+          and travel. During my time at Des Moines Area Community College
+          (DMACC), I pursued an associate degree with a focus on web
+          development. Recognizing the value of design skills in my field, I
+          proactively advocated to enroll in specialized courses within the
+          Graphic Design program—despite them being reserved for program majors.
+        </p>
 
-      <!-- Flex container for content -->
-      <div class="md:flex md:gap-6">
-        <h3 class="w-full py-3 text-2xl bg-[var(--bg)] text-[var(--primary)]">
-          About Me
-        </h3>
-        <!-- Text section -->
-        <div class="bg-[var(--bg)] md:basis-1/2 text-lg p-3">
-          <p>
-            I'm a passionate Web Developer based in Iowa, currently immersed in
-            studying Web Development. Alongside my coursework, I juggle two jobs
-            to support the lavish lifestyle of my two beloved cats. I'm driven
-            by curiosity and creativity, and I look forward to expanding my
-            skills even further—especially once I complete my studies and dive
-            into personal projects that reflect my growth and ambition. I'm
-            looking forward to exploring new opportunities to enhance my skills.
-          </p>
-          <br />
-          <p>
-            I have a strong interest in UI/UX Design, Graphic Design, 3D
-            Modeling, and Traveling.
-          </p>
-        </div>
+        <ul class="my-4 ml-4 space-y-2 text-[var(--secondary)]">
+          <li>InDesign</li>
+          <li>Illustrator</li>
+          <li>Photoshop</li>
+          <li>Typography</li>
+        </ul>
 
-        <!-- Image section -->
-        <div class="md:basis-1/2 flex justify-center items-center mt-6 md:mt-0">
-          <img
-            src="/assets/devin_ledesma.png"
-            alt="image of myself"
-            class="min-w-[300px] max-w-[450px]"
-          />
-        </div>
+        <p>
+          After presenting a strong case for how these courses would enhance my
+          education and career readiness, I was granted permission to take them
+          and successfully completed each one. This experience reinforced my
+          commitment to interdisciplinary learning and my determination to seek
+          out opportunities that align with my goals.
+        </p>
+      </div>
+    </div>
+    <div>
+      <img
+        src="/assets/portfolio_headshot_2025.png"
+        alt="image of myself"
+        class="rounded-2xl"
+      />
+    </div>
+
+    <div
+      class="project-card gap-4 space-y-10 md:col-span-2 md:grid md:grid-cols-3"
+    >
+      <!-- Grid item 1 -->
+      <div>
+        <img
+          src="/assets/626.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Minimalist Movie Poster</h3>
+        <h2>Illustrator</h2>
+        <p>
+          Played around with opacity to create the vision of the opening screen
+          from Lilo Stitch where he licks the inside of his glass prison.
+        </p>
+      </div>
+      <!-- Grid item 2 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_DEVIN_APPLE-TART_Page_2.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Magazine Cover</h3>
+        <h2>Illustrator with AI</h2>
+        <p>
+          The objective was to create a magazine cover utilizing AI that would
+          be visually represent a two-page spread for the recipe (next image).
+        </p>
+      </div>
+      <!-- Grid item 3 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_DEVIN_APPLE-TART_Page_3.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Recipe Page</h3>
+        <h2>Typography + InDesign</h2>
+        <p>
+          By requiring the use of every word, this assignment pushed us to think
+          critically about the layout, strategic use of whitespace, and
+          structure for clear communication.
+        </p>
+      </div>
+      <!-- Grid item 4 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_DEVIN_Movie_Poster_Textured.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Minimalist Movie Cover with Texture</h3>
+        <h2>Typography + Illustrator</h2>
+        <p>
+          After sketching a concept for a movie, we designed a minimalist poster
+          to represent it. As part of the assignment, I created a texture that
+          made the poster look aged—showing folds, wear, and tear to give it a
+          vintage feel.
+        </p>
+      </div>
+      <!-- Grid item 5 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_PLfinals_Page_2.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Advertisement Flyer</h3>
+        <h2>InDesign + Photoshop</h2>
+        <p>
+          This project illustrates how visual hierarchy (z-shaped pattern) and
+          effective use of whitespace can guide the viewer’s attention, enhance
+          readability, and create a clean, balanced layout.
+        </p>
+      </div>
+      <!-- Grid item 6 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_lyrical-layouts.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Lyrical Layout</h3>
+        <h2>Illustrator</h2>
+        <p>
+          For this project, we created a lyrical layout for a song, including
+          the title, author, and full lyrics. The design reflected the chaotic
+          nature of the lyrics—navigating through darkness—with a balanced use
+          of color to guide the viewer and create a sense of depth.
+        </p>
+      </div>
+      <!-- Grid item 7 -->
+      <div>
+        <img
+          src="/assets/LEDESMA_book-cover.png"
+          alt="Illustrator artwork"
+          class="w-full object-cover rounded-md"
+        />
+        <h3>Book Cover with Texture</h3>
+        <h2>Illustrator</h2>
+        <p>
+          This cover visually narrates the story of Peter Pan, inviting the
+          reader to feel as though they’re peering through the pages of the book
+          themselves. The design features a card-style cutout, layering text and
+          illustration to create depth and a sense of immersive storytelling.
+        </p>
       </div>
     </div>
   </section>`,
-  styles: ``,
+  styles: `
+    .project-card div {
+      border: 2px solid var(--text);
+      padding: 1rem;
+      border-radius: 1rem;
+    }
+
+    .project-card h3,
+    .project-card h2,
+    .project-card p {
+      background: var(--bg);
+    }
+
+    .project-card h3 {
+      color: var(--primary);
+      font-size: 1.25rem;
+      padding-bottom: 0.8rem;
+    }
+
+    .project-card h2 {
+      color: var(--secondary);
+      font-size: 0.8rem;
+      padding-bottom: 0.5rem;
+    }
+  `,
 })
 export class AboutComponent {}
